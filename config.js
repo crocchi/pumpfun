51f3eb00f13c270f2e1e522761ac4a36411d1754
crocchi <ziocro@gmail.com>
2025-07-28 21:38:28 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
 import 'dotenv/config';
 
-module.exports = {
-  MORALIS_API_KEY: process.env.MORALIS_API_KEY,
-  //RPC_URL: process.env.RPC_URL,
+export const MORALIS_API_KEY= process.env.MORALIS_API_KEY;
+  
+
+
+//RPC_URL: process.env.RPC_URL,
   //BUY_AMOUNT_SOL: parseFloat(process.env.BUY_AMOUNT_SOL),
   //TAKE_PROFIT: parseFloat(process.env.TAKE_PROFIT),
   //SLIPPAGE: parseFloat(process.env.SLIPPAGE),
-};
+
