8e86d0483c8c7bc75fbf3366af80aa7d5e42142c
crocchi <ziocro@gmail.com>
2025-08-06 21:34:55 +0200

    Update decodeSolana.js

--- utility/decodeSolana.js
+++ utility/decodeSolana.js
@@ -5,14 +5,19 @@ import bs58 from 'bs58';
 const base64String = 'G3KpTd7rY3YWAAAAUGlua3kgcHJvbWlzZSBpdCBzZW5kcwUAAABQaW5reVAAAABodHRwczovL2lwZnMuaW8vaXBmcy9iYWZrcmVpZGRiZ3lpdmVyZ29vb200Ym41bWh3NDZieWVuNWpmN3FoM3p4NHhta3VlaWR4bmZ6cTRzedZocrL43v5V1zwkw/jWcjgn/q744/ge2h9rRj170QUfN87HA6TNtnSr6bCx+7fKV8LfU+ASawS4rjvJkAD0IJyolR6tVisW+AxPKuA7CoTanDY9XlTth5djcLuyB9k8lqiVHq1WKxb4DE8q4DsKhNqcNj1eVO2Hl2Nwu7IH2TyWQ06TaAAAAAAAENhH488DAACsI/wGAAAAAHjF+1HRAgAAgMakfo0DAA==';
 
 export function readString(buffer, offset) {
-  const length = buffer.readUInt32LE(offset);
-  const start = offset + 4;
-  const end = start + length;
-  return {
-    value: buffer.toString('utf8', start, end),
-    bytesRead: 4 + length
-  };
-}
+    const length = buffer.readUInt32LE(offset); // LUNGHEZZA della stringa
+    const start = offset + 4;
+    const end = start + length;
+  
+    if (end > buffer.length) {
+      throw new RangeError(`Trying to read beyond buffer length. Requested: ${end}, Buffer length: ${buffer.length}`);
+    }
+  
+    return {
+      string: buffer.toString('utf8', start, end),
+      offset: end
+    };
+  }
 
 export function decodeProgramData(base64) {
   const buffer = Buffer.from(base64, 'base64');
