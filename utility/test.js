fdf91feab06eef03f1c06dcf08e3b0fa56871ef5
crocchi <ziocro@gmail.com>
2025-08-08 00:18:22 +0200

    Update test.js

--- utility/test.js
+++ utility/test.js
@@ -86,8 +86,17 @@ wshelius.on('message', async (data) => {
     } 
      
       if ( logs.some(line => line.includes("Instruction: Initialize")) && logs.some(line => line.includes("Instruction: InitializeMint2"))){
+        const programData = logs.find(line => line.includes("Program data: "));
+       const dataP = programData?.split("Program data: ")[1];
         console.log("🆕 Token creato su Raydium LaunchLab - letsbonk.fun ");
         console.log("🔗 TX:", `https://solscan.io/tx/${signature}`);
+        try {
+          decoded = decodeProgramData(dataP);
+          console.log("📦 Dati del token:", decoded);
+        } catch (err) {
+          console.error('❌ Failed to decode:', err.message);
+        }
+        console.log("------------------------------");
         //console.log(logs)
       }
 
