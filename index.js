bdce99b7c9b2e14f091a0f9a7d0b729875571fa0
crocchi <ziocro@gmail.com>
2025-09-10 17:48:53 +0200

    up s

--- index.js
+++ index.js
@@ -13,6 +13,24 @@ import { buyToken , sellToken } from './utility/lightTrx.js';
 // Avvia HTTP server
 startHttpServer(process.env.PORT);
 
+import { Server } from 'socket.io';
+import { createServer } from 'http';
+
+const httpServer = createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Server con socket.io');
+});
+
+export const io = new Server(httpServer, {
+  cors: {
+    origin: "*", // permette a tutti i client di connettersi
+  },
+});
+
+httpServer.listen(4000, () => {
+  console.log('🚀 Server in ascolto su http://localhost:4000');
+});
+
 io.on('connection', (socket) => {
   console.log('🔌 Nuovo client connesso:', socket.id);
 
