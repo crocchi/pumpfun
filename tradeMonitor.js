8437cd3bbe094a29ff9fac9124a764205cd63d5f
crocchi <ziocro@gmail.com>
2025-10-28 00:13:04 +0100

    fix

--- tradeMonitor.js
+++ tradeMonitor.js
@@ -26,7 +26,7 @@ class TokenMonitor {
     this.quickSell = 0;
     this.rugpullSafe = true;
     this.prez;
-    this.highPrez= -1;
+    this.highPrez= 0.0000000001;
     this.infoSnipe;
     this.infoSniper;
     //sell percent to sell or trailing
